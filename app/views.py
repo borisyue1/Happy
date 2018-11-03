0b84292c2235887d5844f205ed55a8343f9a330b
Avery Yip <averyyip@berkeley.edu>
2018-11-03 03:49:39 -0700

    Moved filed

--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
 from app import app
 from flask import render_template, flash, redirect, request
 import json
-from scripts.faces import get_emotions
+from .scripts.faces import get_emotions
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route('/new_entry')
+@app.route('/new_entry', methods=["POST"])
 def new_entry():
     return render_template("index.html")
